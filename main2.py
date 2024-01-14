a02aec6b0f5dfb4ad976e5cd0e9fc9b521ae2ed6
Piotr Wojcieszak <piotrekw1002@gmail.com>
2024-01-14 18:21:39 +0100

    Pierwsza działająca wersja

--- main2.py
+++ main2.py
@@ -5,22 +5,19 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
 # Definicje typów kontenerów
-EMPTY, RESIDENTIAL, KITCHEN, SANITARY, COMMON, CORRIDOR = range(6)
-CONTAINER_TYPES = [EMPTY, RESIDENTIAL, KITCHEN, SANITARY, COMMON]
+CORRIDOR, RESIDENTIAL, KITCHEN, SANITARY, COMMON = range(5)
+CONTAINER_TYPES = [CORRIDOR, RESIDENTIAL, KITCHEN, SANITARY, COMMON]
 
 # Wymiary siatki
 GRID_WIDTH, GRID_HEIGHT = 10, 10
 
 # Kolory i etykiety dla wizualizacji
-colors = ['white', 'blue', 'green', 'red', 'yellow', 'grey']
-labels = ['Pusty', 'Mieszkalny', 'Kuchnia', 'Sanitariaty', 'Przestrzeń wspólna', 'Korytarz']
+colors = ['grey', 'blue', 'green', 'red', 'yellow']
+labels = ['Korytarz', 'Mieszkalny', 'Kuchnia', 'Sanitariaty', 'Przestrzeń wspólna']
 cmap = mcolors.ListedColormap(colors)
 
 def fitnessFunction(individual):
     grid = np.array(individual).reshape(GRID_HEIGHT, GRID_WIDTH)
-    
-    # Umieszczenie korytarza
-    placeCorridor(grid)
 
     # Liczba kontenerów różnego typu
     num_residential = np.sum(grid == RESIDENTIAL)
@@ -29,22 +26,24 @@ def fitnessFunction(individual):
     num_sanitary = np.sum(grid == SANITARY)
     
     # Oblicz średnią odległość od kontenerów mieszkalnych do innych typów kontenerów
-    distance_score = calculateAverageDistance(grid)
+    distance_score = calculateAverageDistance(grid) * 2.5
     # Optymalne proporcje
     ideal_residential_per_kitchen = 5
     ideal_residential_per_common = 10
-    ideal_residential_per_sanitary = 8
+    ideal_residential_per_sanitary = 3
 
     # Obliczenie kar za nieoptymalne proporcje
-    kitchen_score = abs(num_residential - ideal_residential_per_kitchen * num_kitchen)
-    common_score = abs(num_residential - ideal_residential_per_common * num_common)
-    sanitary_score = abs(num_residential - ideal_residential_per_sanitary * num_sanitary)
+    kitchen_score = abs(num_residential - ideal_residential_per_kitchen * num_kitchen) * 4
+    common_score = abs(num_residential - ideal_residential_per_common * num_common) * 4
+    sanitary_score = abs(num_residential - ideal_residential_per_sanitary * num_sanitary) * 4
 
     # Ocena dostępności do korytarza
     accessibility_score = evaluateAccessibility(grid)
 
+    residential_score = num_residential * (-15)
+
     # Łączna ocena
-    total_score = kitchen_score + common_score + sanitary_score + accessibility_score + distance_score
+    total_score = kitchen_score + common_score + sanitary_score + accessibility_score + distance_score + residential_score
     return total_score,
 
 def calculateAverageDistance(grid):
@@ -66,78 +65,50 @@ def calculateAverageDistance(grid):
 
 def manhattanDistance(loc1, loc2):
     return abs(loc1[0] - loc2[0]) + abs(loc1[1] - loc2[1])
-def placeCorridor(grid):
-    # Tworzenie krzyżyka przez środek siatki
-    middle_row = GRID_HEIGHT // 2
-    middle_col = GRID_WIDTH // 2
-    for i in range(GRID_HEIGHT):
-        grid[i][middle_col] = CORRIDOR
-    for j in range(GRID_WIDTH):
-        grid[middle_row][j] = CORRIDOR
 
-    # Znajdź kontenery na siatce
-    container_positions = [(i, j) for i in range(GRID_HEIGHT) for j in range(GRID_WIDTH) if grid[i][j] in [RESIDENTIAL, KITCHEN, SANITARY, COMMON]]
-
-    # Łącz kontenery z krzyżykiem, jeśli jeszcze nie są połączone
-    for pos in container_positions:
-        if not isDirectlyConnectedToCorridor(grid, pos[0], pos[1], middle_col):
-            connectContainerWithCorridor(grid, pos[0], pos[1], middle_row, middle_col)
-
-def isDirectlyConnectedToCorridor(grid, i, j, corridor_col):
+def isDirectlyConnectedToCorridor(grid, i, j):
     # Sprawdza, czy dana komórka jest bezpośrednio połączona z korytarzem
-    return grid[i][corridor_col] == CORRIDOR or grid[GRID_HEIGHT // 2][j] == CORRIDOR
+    return (j+1 < len(grid[i]) and grid[i][j+1] == CORRIDOR) or (
+            i+1 < len(grid) and grid[i+1][j] == CORRIDOR ) or (
+            j-1 > 0 and grid[i][j-1] == CORRIDOR) or (
+            i-1 > 0 and grid[i-1][j] == CORRIDOR)
 
-def connectContainerWithCorridor(grid, i, j, middle_row, middle_col):
-    # Połącz kontener z najbliższym segmentem krzyżyka
-    # Rysuj korytarz pionowo lub poziomo do środka, a potem do krzyżyka
-    if abs(i - middle_row) < abs(j - middle_col):
-        # Blizej środkowego rzędu, rysuj pionowo
-        vertical_step = 1 if i < middle_row else -1
-        while i != middle_row:
-            grid[i][j] = CORRIDOR
-            i += vertical_step
-    else:
-        # Blizej środkowej kolumny, rysuj poziomo
-        horizontal_step = 1 if j < middle_col else -1
-        while j != middle_col:
-            grid[i][j] = CORRIDOR
-            j += horizontal_step
+def isCorridorConnectedToCorridor(grid, i, j):
+    # Sprawdza, czy dana komórka korytarza jest połączona z więcej niż jednym korytarzem
+    numberOfNeighbourCorridors = (int(j+1 < len(grid[i]) and grid[i][j+1] == CORRIDOR) + int(
+            i+1 < len(grid) and grid[i+1][j] == CORRIDOR ) + int(
+            j-1 > 0 and grid[i][j-1] == CORRIDOR) + int(
+            i-1 > 0 and grid[i-1][j] == CORRIDOR))
+    logic_value = (numberOfNeighbourCorridors > 1) or \
+                   ((j + 1 > len(grid[i])) or (j - 1 < 0) or (i + 1 > len(grid)) or (i - 1 < 0))
+    return logic_value
 
 
 def evaluateAccessibility(grid):
     accessibility_score = 0
-    corridor_column = GRID_WIDTH // 2
 
     # Sprawdzenie, czy każdy kontener jest połączony z korytarzem
     for i in range(GRID_HEIGHT):
         for j in range(GRID_WIDTH):
             if grid[i][j] in [RESIDENTIAL, KITCHEN, COMMON, SANITARY]:
-                if not isDirectlyConnectedToCorridor(grid, i, j, corridor_column):
-                    accessibility_score += 10  # Duża kara za brak bezpośredniego połączenia
-
-    # Dodatkowa kara za brak ciągłości korytarza
-    if not isCorridorContinuous(grid, corridor_column):
-        accessibility_score += 100
+                if not isDirectlyConnectedToCorridor(grid, i, j):
+                    accessibility_score += 20  # Duża kara za brak bezpośredniego połączenia
+            if grid[i][j] == CORRIDOR:
+                if not isCorridorConnectedToCorridor(grid, i, j):
+                    accessibility_score += 40  # Kara za niepołączone ze sobą korytarze
 
     return accessibility_score
 
-def isDirectlyConnectedToCorridor(grid, i, j, corridor_col):
-    # Sprawdza, czy dana komórka jest bezpośrednio połączona z korytarzem
-    return grid[i, corridor_col] == CORRIDOR
-
-def isCorridorContinuous(grid, corridor_col):
-    # Sprawdza, czy korytarz jest ciągły
-    return all(grid[i, corridor_col] == CORRIDOR for i in range(GRID_HEIGHT))
 
 # ... (reszta kodu)
 
 
 # Inicjalizacja DEAP
-creator.create("FitnessMax", base.Fitness, weights=(-1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMin)
 
 toolbox = base.Toolbox()
-toolbox.register("attr_item", random.choice, CONTAINER_TYPES)
+toolbox.register("attr_item", lambda: CORRIDOR)
 toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_item, GRID_WIDTH * GRID_HEIGHT)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
@@ -163,16 +134,16 @@ def main():
     # Parametry algorytmu ewolucyjnego
     prob_cross = 0.7
     prob_mut = 0.2
-    num_generations = 400
+    num_generations = 500
 
     # Uruchomienie algorytmu ewolucyjnego
-    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=prob_cross, mutpb=prob_mut, 
+    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=prob_cross, mutpb=prob_mut,
                                    ngen=num_generations, stats=stats, 
                                    halloffame=hof, verbose=True)
 
     # Wizualizacja najlepszego rozwiązania
     best = np.array(hof[0]).reshape(GRID_HEIGHT, GRID_WIDTH)
-    placeCorridor(best)  # Umieszczenie korytarza przed wizualizacją
+    # placeCorridor(best)  # Umieszczenie korytarza przed wizualizacją
     print("Najlepsze znalezione rozwiązanie:")
     print(best)
     plotGrid(best)
